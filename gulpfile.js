65dd095a7c75bb2dae6a994a97472d45d97a7bc6
Neo <mryangjian@live.com>
2017-09-02 21:26:13 +0800

    hello

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var gulp = require('gulp');
+var electron = require('electron-connect').server.create();
+
+gulp.task('serve', function () {
+
+  // Start browser process
+  electron.start();
+
+  // Restart browser process
+  gulp.watch('main.js', electron.restart);
+
+  // Reload renderer process
+  gulp.watch(['js/*.js', 'index.html'], electron.reload);
+});
+
+gulp.task('default',['serve']);
